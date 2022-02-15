c2bc555cc57fba2512bf7d161190473ceccdc788
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2022-02-15 17:01:18 +0300

    get access and refresh tokens

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const signUp = require('./sign-up');
-const signIn = require('./sign-in')
+const signIn = require('./sign-in');
+const bodyParser = require('body-parser')
 const { validate } = require('../../utils');
 
 router
--- server/routes/auth/sign-in.js
+++ server/routes/auth/sign-in.js
@@ -2,21 +2,24 @@ require('dotenv').config()
 
 const { body } = require('express-validator');
 const { User } = require('../../../models')
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
 
 const action = async (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password
-  };
-  await User.findOne({user})
-    .then((data) => {
-        res.status(200).send({
-          firstName: data.firstName, 
-          lastName: data.lastName,
-          id: data.id
-        });
-    })
-    .catch(err=>console.log(err));
+  const user = await User.findOne({
+    where: { 
+        email: req.body.email
+    }
+  });
+  if (!user) {
+    const error = new Error();
+    error.status = 403;
+    throw error;
+  }
+  const refreshToken = uuidv4();
+  const accessToken = jwt.sign({id: user.id}, process.env.SECRET, { expiresIn: '1800s' });
+  res.json({accessToken: accessToken, refreshToken:refreshToken});
+  
 };
 
 const validators = [
