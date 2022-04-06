5352345edf8541e7027991a992e3ffa8cf50219f
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2022-04-06 14:43:27 +0300

    sort tag work

--- server/routes/posts/posts-tag.js
+++ server/routes/posts/posts-tag.js
@@ -3,9 +3,25 @@ const { Post, User, Tag} = require('../../../models')
 const action = async (req, res) => {
   const tag = req.params.tag;
   
-  const tagDB = await Tag.findOne({
+  const tagDB = await Tag.findAll({
     where: {tag: tag},
-    
+    include: [
+      {
+        model: Post,
+        as: 'Posts',
+        include: [
+          {
+            model: User,
+            as:'Users'
+          },
+          {
+            model: Tag,
+            as: 'Tag'
+          },
+
+        ]
+      },
+    ]
     })
     .then((data) => {
       res.send(data);
@@ -13,7 +29,7 @@ const action = async (req, res) => {
     .catch((err) => {
       res.send('ERROR, not found');
     });
-    // res.send(tagDB);
+    
     
     
   
