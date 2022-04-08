61e6c7e9eaf6d5f5beaa2b80b5037c94a48cdc57
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2022-04-08 17:52:38 +0300

    start google log in

--- server/app.js
+++ server/app.js
@@ -8,25 +8,89 @@ const path = require('path')
 const app = express();
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oidc');
+const session = require('express-session');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+passport.use(new GoogleStrategy({
+  clientID: process.env['GOOGLE_CLIENT_ID'],
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: 'http://localhost:3000/api/auth/google/callback'
+},
+function(issuer, profile, cb) {
+  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+    issuer,
+    profile.id
+  ], function(err, cred) {
+    if (err) { return cb(err); }
+    if (!cred) {
+      // The Google account has not logged in to this app before.  Create a
+      // new user record and link it to the Google account.
+      db.run('INSERT INTO users (name) VALUES (?)', [
+        profile.displayName
+      ], function(err) {
+        if (err) { return cb(err); }
+
+        var id = this.lastID;
+        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+          id,
+          issuer,
+          profile.id
+        ], function(err) {
+          if (err) { return cb(err); }
+          var user = {
+            id: id.toString(),
+            name: profile.displayName
+          };
+          return cb(null, user);
+        });
+      });
+    } else {
+      // The Google account has previously logged in to the app.  Get the
+      // user record linked to the Google account and log the user in.
+      db.get('SELECT * FROM users WHERE id = ?', [ cred.user_id ], function(err, user) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        return cb(null, user);
+      });
+    }
+  });
+}
+));
+
 app.use(cors({
+
   credentials: true,
 }))
-let allowedOrigins = ['http://localhost:3000',
-                      'http://localhost:4200'];
+let allowedOrigins = ['http://localhost:4200',
+                      'http://localhost:3000',
+                    '*'];
 app.use( bodyParser({limit: '50mb'}) );
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+  
+//   origin: 
+//   function(origin, callback){
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       res.header('Access-Control-Allow-Origin', '*');
+//       const msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+
+
 
 app.withCredentials = true;
 app.use(express.static(path.join(__dirname, 'public')));
--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -3,7 +3,9 @@ const signUp = require('./sign-up');
 const signIn = require('./sign-in');
 const getUser = require('./get-user');
 const refreshToken = require('./refresh-token');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oidc');
 const { validate } = require('../../utils');
 
 
@@ -11,6 +13,13 @@ router
   .get('/refresh-token', refreshToken.action)
   .get('/', getUser.action)
   .post('/sign-up', validate(signUp.validators), signUp.action)
-  .post('/sign-in', validate(signIn.validators), signIn.action);
-
+  .post('/sign-in', validate(signIn.validators), signIn.action)
+  .get('/google', passport.authenticate('google', { scope: ['profile'] }))
+  .get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' })
+  // ,
+  //   function(req, res) {
+  //     // Successful authentication, redirect home.
+  //     res.redirect('/');
+  //   }
+  );
 module.exports = router;
