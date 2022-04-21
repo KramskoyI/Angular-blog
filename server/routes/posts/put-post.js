883f2ac2866b40e2632ea755b62fe01837d7864d
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2022-04-21 15:00:13 +0300

    edit image and tag post

--- server/routes/posts/put-post.js
+++ server/routes/posts/put-post.js
@@ -1,19 +1,87 @@
-const { Post } = require('../../../models')
+const { Post, Tag } = require('../../../models')
+const fs = require("fs");
 
-const action = (req, res) => {
-  Post.update(
-    req.body,
-    {
-      where: { id: req.params.id },
-    },
-    )
-    .then((data) => {
-      res.status(200).send(data);
-    })
-    .catch(() => {
-      res.status(500).send({ msg: err });
+const action = async (req, res) => {
+  const data = req.body.image;
+  const tag =req.body.tag;
+  console.log(req.body)
+  if(data) {
+    const base64Data = data.replace( /^data:image\/jpeg;base64,/, "");
+    const file = req.body.nameImage;
+    fs.writeFile(file, base64Data, 'base64', function(err) {
+      if(err) {
+        console.log(err);
+      } 
     });
-  
+    fs.rename( file, `server/public/image/${file}`, err => {
+      if(err) throw err; // не удалось переместить файл
+      console.log('Файл успешно перемещён');
+    });
+      
+    const post = {
+      title: req.body.title,
+      content: req.body.content,
+      image: file,
+    };
+      
+    await Post.update(
+      post,
+      {
+        where: { id: req.params.id },
+      }
+    )
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch(() => {
+        res.status(500);
+      });
+      
+      
+    } else {
+      const post = {
+        title: req.body.title,
+        content: req.body.content,
+        image: 'not.jpeg',
+        autorId: req.body.autorId
+      };
+      
+      const postDb = await Post.update(
+        post,
+        {
+          where: { id: req.params.id },
+        },
+        )
+        .then((data) => {
+          res.status(200).send(data);
+        })
+        .catch(() => {
+          res.status(500);
+        });
+    }
+    if(tag) {
+      Tag.destroy({
+        where: { postId: req.params.id},
+      })
+      .then((data) => {
+        res.status(200).end();
+      })
+      .catch(() => {
+        res.status(500);
+      });
+      const arrTags = tag.split(',');
+      console.log(arrTags)
+      for (let i = 0; i < arrTags.length; i++) {
+        let tagOne = {
+          tag: arrTags[i],
+          postId: req.params.id
+        };
+        Tag.create(tagOne)
+        .then(()=>{
+          res.status(200)
+        })
+      }
+    }
 };
 
 
